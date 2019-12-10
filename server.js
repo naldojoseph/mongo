51d4f5eddef2a53272fbd2ba622e61a2808a2d9c
Naldo Tumundo <naldo_j3@yahoo.com>
2019-12-09 18:34:12 -0800

    fixx mongo

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const bodyParser = require("body-parser"); //JSON responses
 const mongoose = require("mongoose"); //Mongo object modelling 
-const request = require("request"); //Makes http calls
-const cheerio = require("cheerio"); //Scraper
+//Place these after you load the mongoose package into you variable mongoose
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 // Require all models
 const db = require("./models");
@@ -28,12 +31,11 @@ app.use(express.static("public"));
 const router = require("./controllers/api.js");
 app.use(router);
 // Connect to the Mongo DB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:8080/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI,);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
 // mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
